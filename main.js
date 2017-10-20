87cad56a897e8385b8f8c37c681444c8bf1c0416
Cansen20 <csen@newroads.org>
2017-10-20 09:28:38 -0700

    i made it so that the magic 8 bal l changes the answer when you click on it and i changed my code a lot and I also put in some more diolouge and did everything that you told us to do

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+var list = ["we shall see ",
+			"yes",
+			"no" ,
+			"ask again later",
+			"maybe",
+			"signs point to yes",
+			"as is see it, yes",
+			"certainly",
+			"doubtful" ,
+			"try again later"];
 
-
-function testjs () {
-	alert("text ") ;
+function randomNumber(){
+	return Math.floor (Math.random()* 10);
 }
 
-function shake8ball () {
+				//just testing javascript
+function testJs() {
+	alert("test") ;
+}
+
+function shake8ball() {
 	document.getElementById("eightball").src="magic8ball.jpg"; 
-	alert ("we shall see!");
+	//alert ("we shall see!"); 
+
+	//change paragraph to magic 8 ball text
+	document.getElementById("results").innerHTML=list[randomNumber()]; 
+
+	//alert(RandomNumber()) ;
 }
