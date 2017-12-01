95d828b3f826c7c4c3b3a046047e45ca82be7e07
Johnsen02 <csen@newroads.org>
2017-12-01 09:54:05 -0800

    got almost everything, the x's and o's do not work yet

--- main.js
+++ main.js
@@ -10,10 +10,39 @@ function shake8ball () {
 } 
 
 /* 
-Below is the code for tic tac toe
+
+Below is the code for tic tac toe 
+
 */ 
 
-//handles x and o signs 
+//holdes x or o
+var character ="o";
+
+//handles x and o turns 
 function turn(location) {
-	document.getElementById(location).innerHTML="X";
+	document.getElementById(location).innerHTML = character;
+	if(character == "x"){
+		character = "o"; 
+		}
+		else {
+		character
+		
+		}
+
+
+
 }
+
+//clears x's and o's 
+function clearAll (){
+	document.getElementById("r1c1").innerHTML = "";
+	document.getElementById("r1c2").innerHTML = "";
+	document.getElementById("r1c3").innerHTML = "";
+	document.getElementById("r2c1").innerHTML = "";
+	document.getElementById("r2c2").innerHTML = "";
+	document.getElementById("r2c3").innerHTML = "";
+	document.getElementById("r3c1").innerHTML = "";
+	document.getElementById("r3c2").innerHTML = "";
+	document.getElementById("r3c3").innerHTML = "";
+}
+
