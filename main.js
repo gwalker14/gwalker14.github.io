250943e35b662a8d48afedf01e7b9f82e2766e4b
gwalker14 <gwalker@newroads.org>
2017-12-08 09:23:51 -0800

    i got the alert to work and everything else we did today in class. but my restart doesn't work

--- main.js
+++ main.js
@@ -10,39 +10,60 @@ function shake8ball () {
 } 
 
 /* 
-
-Below is the code for tic tac toe 
-
+	Below is the code for tic tac toe 
 */ 
 
-//holdes x or o
-var character ="o";
+//holdes X or O
+var character ="O";
 
-//handles x and o turns 
+//handles X and O turns 
 function turn(location) {
-	document.getElementById(location).innerHTML = character;
-	if(character == "x"){
-		character = "o"; 
+	//if no X or O than draw
+	if(document.getElementById(location).innerHTML == "")
+		//switch between X and O
+		if(character == "X"){
+			character = "O"; 
 		}
-		else {
-		character
+		else{
+				character="X";
+			
+		}
 		
-		}
+			document.getElementById(location).innerHTML = character;
 
-
-
+		checkWinner();
 }
 
-//clears x's and o's 
+//clears X's and O's 
 function clearAll (){
-	document.getElementById("r1c1").innerHTML = "";
-	document.getElementById("r1c2").innerHTML = "";
-	document.getElementById("r1c3").innerHTML = "";
-	document.getElementById("r2c1").innerHTML = "";
-	document.getElementById("r2c2").innerHTML = "";
-	document.getElementById("r2c3").innerHTML = "";
-	document.getElementById("r3c1").innerHTML = "";
-	document.getElementById("r3c2").innerHTML = "";
-	document.getElementById("r3c3").innerHTML = "";
+	document.getElementById("r1c1").innerHTML;
+	document.getElementById("r1c2").innerHTML;
+	document.getElementById("r1c3").innerHTML;
+	document.getElementById("r2c1").innerHTML;
+	document.getElementById("r2c2").innerHTML;
+	document.getElementById("r2c3").innerHTML;
+	document.getElementById("r3c1").innerHTML;
+	document.getElementById("r3c2").innerHTML;
+	document.getElementById("r3c3").innerHTML;
+} 
+
+function checkWinner() {
+
+	 
+
+	var r1c1 = document.getElementById("r1c1").innerHTML;
+	var r1c2 = document.getElementById("r1c2").innerHTML;
+	var r1c3 = document.getElementById("r1c3").innerHTML;
+	var r2c1 = document.getElementById("r2c1").innerHTML;
+	var r2c2 = document.getElementById("r2c2").innerHTML;
+	var r2c3 = document.getElementById("r2c3").innerHTML;
+	var r3c1 = document.getElementById("r3c1").innerHTML;
+	var r3c2 = document.getElementById("r3c2").innerHTML;
+	var r3c3 = document.getElementById("r3c3").innerHTML;
+
+	if(r1c1 == r1c2 && r1c1 == r1c3 && r1c1 !=""){
+		
+		alert("Winner!");
+	}
 }
 
