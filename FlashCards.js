e4ea823799914a6f5f47c275b634330eb3a8fe0a
https://github.com/gwalker14/gwalker14.github.io.git <https://github.com/gwalker14/gwalker14.github.io.git>
2018-01-26 09:40:42 -0800

    I got the numbers to work and i git 2 plus 3 to be 5.

---
+++ FlashCards.js
@@ -0,0 +1,14 @@
+// addition opperation 
+function addNumbers() {
+   //get first number of textbox 
+  var opreand1 = document.getElementById('operand1').value;
+  var opreand2 = document.getElementById('operand2').value;
+
+   //add to second number of second textbox
+  var sum =parseInt (opreand1) + parseInt (opreand2);
+
+  //test sum
+  alert(sum);
+
+   //check if the sum equals the answer textbox
+}
