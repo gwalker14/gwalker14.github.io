baca9f0324ae08405ac7f97cc599fd695751d37a
gwalker14 <gwalker@newroads.org>
2018-04-20 09:40:18 -0700

    I tried to change around my website to make it better. I also made my snake game better, by ading text under the game to make it look better

--- Snake.js
+++ Snake.js
@@ -34,17 +34,3 @@ var drawModule = (function () {
         ctx.fillStyle = 'blue';
         ctx.fillText(score_text, 145, h-5);
     } 
-
-    var game = "play"; //This is the game variable   
-
-$("#Restart").click(function() { //Using the buttons        
-            console.log("Restart Works"); 
-            gamerestart();
-            ctx.fill
-    });    
-
-function gamerestart(){
-        game = gamerestart;
-        ctx.fillStyle = '#00008b';
-        ctx.fillRect(0,0, canvas.width, canvas.height);
-    }
